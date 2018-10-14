e75fff8787e0c629b281f11e10149240263b113f
takashi kawaai <k.takashi0309@gmail.com>
2018-10-14 23:48:40 +0900

    save

--- InstallPackage.ps1
+++ InstallPackage.ps1
@@ -1,4 +1,4 @@
-$packageList =@(
+$chocolateyPackageList =@(
     'git',
     'GoogleChrome',
     'visualstudiocode',
@@ -6,8 +6,19 @@ $packageList =@(
     'nodejs'
 )
 
-$packageList|%{Install-Package $_ -Force -ProviderName ChocolateyGet}
-
+$chocolateyPackageList|%{Install-Package $_ -Force -ProviderName ChocolateyGet}
 
 $Env:Path = [Environment]::GetEnvironmentVariable('Path','Machine') +';'+ [Environment]::GetEnvironmentVariable('Path','User')
 
+$npmPackageList=@(
+    'eslint-cli'
+)
+
+$npmPackageList|%{npm install -g $_}
+
+$vscodeExtentionList=@(
+    'MS-CEINTL.vscode-language-pack-ja',
+    'vscodevim.vim'
+)
+
+$vscodeExtentionList|%{code --install-extension $_}
