10074e99d23d2a829ad8decdbe0f67eecc55e330
takashi kawaai <k.takashi0309@gmail.com>
2018-10-14 16:18:50 +0900

    save

--- InstallModule.ps1
+++ InstallModule.ps1
@@ -1,6 +1,6 @@
 Import-Module PackageManagement
 Install-PackageProvider NuGet -Force
 Install-PackageProvider ChocolateyGet -Force
-
-Install-Module PSReadLine -Force
+Install-Module PowerShellGet -Force
 Install-Module posh-git -Force
+Install-Module PSReadline -AllowPrerelease -Force -SkipPublisherCheck
